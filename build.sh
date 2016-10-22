56ffc3c10e976232ee154bc6d3db6eb4701eeb9a
James Christopher Adduono <jc@adduono.com>
2016-10-22 07:13:59 -0400

    Use firmware copying to fix out of tree builds
    Change-Id: Idbaa2c920bd6c363c453262fa7d3fe350d26aaba

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ fi
 
 echo "setting up build..."
 mkdir "$BUILD"
-make O="$BUILD" device_lge_h990
+make O="$BUILD" elsa_global_com-vanilla_defconfig
 
 echo "building kernel..."
 time make O="$BUILD" -j$NPR 2>&1 |tee ../compile.log
