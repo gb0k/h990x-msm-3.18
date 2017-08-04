9b556a7beba532e674843614213afd5bf0a655f7
Elliott Mitchell <ehem+android_kernel_lge_msm8996@m5p.com>
2017-08-03 20:48:52 -0700

    Add support for reading modem/SIM information from command-line
    Since the device name and SIM count are available from the command-line,
    add support for loading them from the command-line.  I thought there was
    supposed to be a parsed command-line lying around, but the functions to
    use are unknown to me.  Suboptimal, but this DOES work.  The fixing
    values can still be set in the kernel configuration file.
    
    TODO: The actual value capturing in dirtysanta_fixup_loadcfg() is written
    to be moved to kernel initialization time.  This one function call should
    be moved there.  In theory there is a race condition, but the likelyhood
    of triggering that is extremely low and if triggered they should cause
    the same values to be written.

--- drivers/platform/msm/ipa/dirtysanta_fixup.c
+++ drivers/platform/msm/ipa/dirtysanta_fixup.c
@@ -12,6 +12,7 @@
  */
 
 
+#include <linux/init.h>
 #include <linux/string.h>
 #include <linux/printk.h>
 #include <linux/err.h>
@@ -20,13 +21,16 @@
 #include "dirtysanta_fixup.h"
 
 
+#define LG_MODEL_NAME_SIZE 22
+
+
 struct lge_smem_vendor0 {
 	// following 2 fields are common to all devices
 	int hw_rev;
 	char model_name[10]; // "MSM8996_H1"
 	// following fields depend on the device
 	char nt_code[2048];
-	char lg_model_name[22];
+	char lg_model_name[LG_MODEL_NAME_SIZE];
 	int sim_num;
 	int flag_gpio;
 
@@ -36,8 +40,52 @@ struct lge_smem_vendor0 {
 
 static char last[32]="";
 
-static const char dev_name[]=CONFIG_DIRTYSANTA_FIXUP_DEVICENAME;
-static const char sim_num=CONFIG_DIRTYSANTA_FIXUP_SIMCOUNT;
+static char dev_name[LG_MODEL_NAME_SIZE]=CONFIG_DIRTYSANTA_FIXUP_DEVICENAME;
+static char sim_num=CONFIG_DIRTYSANTA_FIXUP_SIMCOUNT;
+
+
+bool dirtysanta_fixup_loadcfg(void)
+{
+	const char MODELNAMEEQ[]="model.name=";
+	const char SIMNUMEQ[]="lge.sim_num=";
+	char *_dev_name;
+	int dev_name_len;
+	char *_sim_num;
+
+	if(!dev_name[0]) {
+		if(!(_dev_name=strstr(saved_command_line, MODELNAMEEQ))) {
+			pr_err("DirtySanta: \"%s\" not passed on kernel command-line\n", MODELNAMEEQ);
+			return 0;
+		}
+		_dev_name+=strlen(MODELNAMEEQ);
+		dev_name_len=strchrnul(_dev_name, ' ')-_dev_name;
+
+		if(dev_name_len>=LG_MODEL_NAME_SIZE) {
+			pr_warning("DirtySanta: model.name is longer than VENDOR0 buffer, truncating!\n");
+			dev_name_len=LG_MODEL_NAME_SIZE-1;
+		}
+
+		memcpy(dev_name, _dev_name, dev_name_len);
+		dev_name[dev_name_len]='\0';
+	}
+
+	if(!sim_num) {
+		if(!(_sim_num=strstr(saved_command_line, SIMNUMEQ))) {
+			pr_err("DirtySanta: \"%s\" not passed on kernel command-line\n", SIMNUMEQ);
+			return 0;
+		}
+		sim_num=_sim_num[strlen(SIMNUMEQ)]-'0';
+
+		if(sim_num<1||sim_num>2)
+			pr_warning("DirtySanta: SIM count of %d is odd\n", sim_num);
+	}
+
+	pr_info("DirtySanta: values: \"%s%s\" \"%s%d\"\n", MODELNAMEEQ,
+dev_name, SIMNUMEQ, sim_num);
+
+	return 1;
+}
+EXPORT_SYMBOL(dirtysanta_fixup_loadcfg);
 
 
 void dirtysanta_fixup_msm_modem(const char *caller)
@@ -49,24 +97,26 @@ void dirtysanta_fixup_msm_modem(const char *caller)
 	unsigned size;
 
 	if(IS_ERR_OR_NULL(ptr=smem_get_entry(SMEM_ID_VENDOR0, &size, 0, SMEM_ANY_HOST_FLAG))) {
-		pr_info("Qualcomm smem not initialized as of \"%s()\"", caller);
+		pr_info("DirtySanta: Qualcomm smem not initialized as of \"%s()\"\n", caller);
 		return;
 	}
 
 	if(size<sizeof(struct lge_smem_vendor0)) {
-		pr_err("Memory area returned by smem_get_entry() too small, when called by \"%s()\"", caller);
+		pr_err("DirtySanta: Memory area returned by smem_get_entry() too small, when called by \"%s()\"\n", caller);
 		return;
 	}
 
-	if(!strcmp(ptr->lg_model_name, dev_name)) {
-		if(ptr->sim_num==sim_num) {
-			pr_info("Previous overwrite of VENDOR0 (\"%s()\") still effective.", last);
-			return;
-		}
+	if(!sim_num||!dev_name[0]) {
+		if(!dirtysanta_fixup_loadcfg()) return;
 	}
 
-	if(last[0]) msg=KERN_INFO "Needing to overwrite VENDOR0 a second time from \"%s()\" (last was \"%s()\")";
-	else msg=KERN_INFO "Overwriting VENDOR0 model name first time from \"%s()\"";
+	if(!strcmp(ptr->lg_model_name, dev_name)&&ptr->sim_num==sim_num) {
+		pr_info("DirtySanta: Previous overwrite of VENDOR0 (\"%s()\") still effective.\n", last);
+		return;
+	}
+
+	if(last[0]) msg=KERN_INFO "DirtySanta: Needing to overwrite VENDOR0 a second time from \"%s()\" (last was \"%s()\")\n";
+	else msg=KERN_INFO "DirtySanta: Overwriting VENDOR0 model name first time from \"%s()\"\n";
 
 	printk(msg, caller, last);
 
