0beb9ffe5f82fb93ccf6a236094bf8e6165b4732
Dickson Lee <nijiwater@live.cn>
2017-09-11 10:39:37 +0800

    exfat: add exfat-nofuse

---
+++ build.sh
@@ -0,0 +1,37 @@
+export KBUILD_BUILD_USER=Dixon
+export KBUILD_BUILD_HOST=ArchLinux
+export ARCH=arm64
+export CROSS_COMPILE=/home/dixon/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+DIR=$(pwd)
+BUILD="$DIR/build"
+OUT="$DIR/out"
+NPR=`expr $(nproc) + 1`
+
+echo "cleaning build..."
+if [ -d "$BUILD" ]; then
+rm -rf "$BUILD"
+fi
+if [ -d "$OUT" ]; then
+rm -rf "$OUT"
+fi
+
+echo "setting up build..."
+mkdir "$BUILD"
+make O="$BUILD" device_lge_h990
+
+echo "building kernel..."
+time make O="$BUILD" -j$NPR 2>&1 |tee ../compile.log
+
+echo "building modules..."
+make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
+rm $BUILD/lib/modules/*/build
+rm $BUILD/lib/modules/*/source
+
+mkdir -p $OUT/modules
+mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
+find "$BUILD/lib/modules/" -name *.ko | xargs -n 1 -I '{}' mv {} "$OUT/modules"
+
+echo "Image.gz-dtb and modules can be found in $OUT"
+
+
