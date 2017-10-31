790c3446d2f02e7e9f67b642cf0c8d3500519cb3
Alucard24 <dmbaoh2@gmail.com>
2017-10-31 13:31:56 +0100

    Added CPU governor : Nightmare
    A PegasusQ modified, less aggressive and more stable. A good compromise between performance and battery.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -507,6 +507,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
 #endif
 
 /*********************************************************************
