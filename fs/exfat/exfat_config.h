c648d17493101fca7550e832568df4ec25d49e58
Dickson Lee <nijiwater@live.cn>
2017-11-22 13:35:27 +0800

    Revert "exfat : Update exFAT to jcadduono/linux_fs_exfat"
    This reverts commit 5f4eecf579b3803b0b62c22357a444f6ab77104d.

--- fs/exfat/exfat_config.h
+++ fs/exfat/exfat_config.h
@@ -19,8 +19,8 @@
 /************************************************************************/
 /*                                                                      */
 /*  PROJECT : exFAT & FAT12/16/32 File System                           */
-/*  FILE    : exfat_blkdev.h                                            */
-/*  PURPOSE : Header File for exFAT Block Device Driver Glue Layer      */
+/*  FILE    : exfat_config.h                                            */
+/*  PURPOSE : Header File for exFAT Configuable Policies                */
 /*                                                                      */
 /*----------------------------------------------------------------------*/
 /*  NOTES                                                               */
@@ -32,42 +32,38 @@
 /*                                                                      */
 /************************************************************************/
 
-#ifndef _EXFAT_BLKDEV_H
-#define _EXFAT_BLKDEV_H
+#ifndef _EXFAT_CONFIG_H
+#define _EXFAT_CONFIG_H
 
-#include <linux/fs.h>
-#include "exfat_config.h"
-
-/*----------------------------------------------------------------------*/
-/*  Constant & Macro Definitions (Non-Configurable)                     */
-/*----------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------*/
-/*  Type Definitions                                                    */
-/*----------------------------------------------------------------------*/
-
-typedef struct __BD_INFO_T {
-	s32 sector_size;      /* in bytes */
-	s32 sector_size_bits;
-	s32 sector_size_mask;
-	s32 num_sectors;      /* total number of sectors in this block device */
-	bool  opened;           /* opened or not */
-} BD_INFO_T;
+/*======================================================================*/
+/*                                                                      */
+/*                        FFS CONFIGURATIONS                            */
+/*                  (CHANGE THIS PART IF REQUIRED)                      */
+/*                                                                      */
+/*======================================================================*/
 
 /*----------------------------------------------------------------------*/
-/*  External Variable Declarations                                      */
+/* Feature Config                                                       */
 /*----------------------------------------------------------------------*/
+#ifndef CONFIG_EXFAT_DISCARD
+#define CONFIG_EXFAT_DISCARD		1	/* mount option -o discard support */
+#endif
 
-/*----------------------------------------------------------------------*/
-/*  External Function Declarations                                      */
-/*----------------------------------------------------------------------*/
+#ifndef CONFIG_EXFAT_DELAYED_SYNC
+#define CONFIG_EXFAT_DELAYED_SYNC 0
+#endif
 
-s32 bdev_init(void);
-s32 bdev_shutdown(void);
-s32 bdev_open(struct super_block *sb);
-s32 bdev_close(struct super_block *sb);
-s32 bdev_read(struct super_block *sb, sector_t secno, struct buffer_head **bh, u32 num_secs, s32 read);
-s32 bdev_write(struct super_block *sb, sector_t secno, struct buffer_head *bh, u32 num_secs, s32 sync);
-s32 bdev_sync(struct super_block *sb);
+#ifndef CONFIG_EXFAT_KERNEL_DEBUG
+#define CONFIG_EXFAT_KERNEL_DEBUG	1	/* kernel debug features via ioctl */
+#endif
 
-#endif /* _EXFAT_BLKDEV_H */
+#ifndef CONFIG_EXFAT_DEBUG_MSG
+#define CONFIG_EXFAT_DEBUG_MSG		0	/* debugging message on/off */
+#endif
+
+#ifndef CONFIG_EXFAT_DEFAULT_CODEPAGE
+#define CONFIG_EXFAT_DEFAULT_CODEPAGE	437
+#define CONFIG_EXFAT_DEFAULT_IOCHARSET	"utf8"
+#endif
+
+#endif /* _EXFAT_CONFIG_H */
